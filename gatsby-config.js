5ec0a8d1855d7df476e00badaa96fc9f22db70fd
Rinkesh Gorasia <reachrinkesh@gmail.com>
2023-03-15 20:18:33 +0530

    Updated gatsby-config.js
    Changed blog and author name

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Rinkesh Gorasia's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Rinkesh Gorasia`,
+      summary: `who lives and works in Banglore building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `rinks__g`,
     },
   },
   plugins: [
